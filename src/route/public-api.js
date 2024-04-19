475b4e730fbd3424da859ae90a32e4f7e69c989b
root <root@srv511009.hstgr.cloud>
2024-04-19 17:32:47 +0000

    Handle preFlight Network

--- src/route/public-api.js
+++ src/route/public-api.js
@@ -75,39 +75,6 @@ publicRouter.put('/api/wilayah', wilayah.update);
 publicRouter.delete('/api/wilayah', wilayah.remove);
 publicRouter.get('/api/wilayah/search', wilayah.search); 
 
-var socket
-function socketConnectionRequest(req, res, next) {
-    const headers = {
-        'Access-Control-Allow-Origin': 'https://admin.synchronice.id', // To tell client, it is allowed to access this resource from any origin
-        'Cache-Control': 'no-cache', // To tell client, it is not a cacheable response
-        'Content-Type': 'text/event-stream', // To tell client, it is event stream
-        'Connection': 'keep-alive', // To tell client, not to close connection
-    };
-    res.writeHead(200, headers);
-    res.write('data: Connection Established, We\'ll now start receiving messages from the server.\n')
-    socket = res
-    console.log('New connection established')
-}
-
-function publishMessageToConnectedSockets(data) {
-    socket.write(`data: ${data}\n`);
-}
-
-publicRouter.get('/status-server', (req, res) => {
-    const status = {
-        "status": "Server is running."
-    }
-
-    res.status(200).json({
-        status
-    });
-})
-publicRouter.get('/socket-connection-request', socketConnectionRequest);
-publicRouter.get('/send-message-to-client', (req, res, next) => {
-    publishMessageToConnectedSockets(`Sukses terus Saptakarya at ${new Date()}`)
-    res.status(200)
-});
-
 export {
     publicRouter
 }
