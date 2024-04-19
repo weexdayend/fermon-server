07e6d80e82fa958f0c337820ec35678be65003b3
root <root@srv511009.hstgr.cloud>
2024-04-19 15:22:36 +0000

    Handle preFlight Network

--- src/application/web.js
+++ src/application/web.js
@@ -6,22 +6,18 @@ import {errorMiddleware} from "../middleware/error-middleware.js";
 
 export const web = express();
 
-web.use(cors());
+web.use(cors(
+    cors({
+        origin: "https://admin.synchronice.id", // restrict calls to those this address
+        methods: "GET" // only allow GET requests
+    })
+));
 
 web.use(express.json());
 
-web.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://admin.synchronice.id");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-});
-
 function socketConnectionRequest(req, res, next) {
     const headers = {
-        'Access-Control-Allow-Origin': 'https://admin.synchronice.id', // To tell client, it is allowed to access this resource from any origin
+        'Access-Control-Allow-Origin': '*', // To tell client, it is allowed to access this resource from any origin
         'Cache-Control': 'no-cache', // To tell client, it is not a cacheable response
         'Content-Type': 'text/event-stream', // To tell client, it is event stream
         'Connection': 'keep-alive', // To tell client, not to close connection
@@ -36,8 +32,15 @@ function publishMessageToConnectedSockets(data) {
     socket.write(`data: ${data}\n`);
 }
 
+web.get('/', (req, res) => {
+    const status = {
+        "status": "Server is running."
+    }
+
+    res.send(status);
+})
 web.get('/socket-connection-request', socketConnectionRequest);
-web.post('/send-message-to-client', (req, res, next) => {
+web.get('/send-message-to-client', (req, res, next) => {
     publishMessageToConnectedSockets(`Sukses terus Saptakarya at ${new Date()}`)
     res.sendStatus(200)
 });
