2c31277448359ae0e1472c90a12a734c5ca61eda
root <root@syncronice.id>
2024-04-19 09:04:02 +0000

    fixing env

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,16 @@
 FROM node:alpine
 WORKDIR /usr/app
+
+# Copy package.json and install dependencies
 COPY package.json .
 RUN npm install
+
+# Copy .env file
+COPY .env .env
+
+# Copy the rest of the application source code
 COPY . .
+
 EXPOSE 4000
+
 CMD ["npm", "run", "start"]
--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 import {web} from "./application/web.js";
 import {logger} from "./application/logging.js";
 
+web.get('/', (req, res) => {
+    res.send("Server is Running")
+    logger.info("Server is Running")
+})
+
 web.listen(4000, () => {
     logger.info("App start");
 });
