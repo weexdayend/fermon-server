3334633b94db2c7a4e126bbd28c01b19c61cdad2
Asep Wahyudi <aseppwahyudii@gmail.com>
2024-04-19 15:38:35 +0700

    Update service.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import {web} from "./src/application/js";
 import {logger} from "./src/application/logging.js";
 
-web.listen(3000, () => {
+web.listen(4000, () => {
     logger.info("App start");
 });
