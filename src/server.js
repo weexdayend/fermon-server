f1ec21a1af86388dc89818603c389644e352f5fa
root <root@syncronice.id>
2024-04-19 09:24:02 +0000

    Testing on serverjs to show port

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ import {web} from "./application/web.js";
 import {logger} from "./application/logging.js";
 
 web.get('/', (req, res) => {
-    res.send("Server is Running")
-    logger.info("Server is Running")
+    res.send("Server is Running on port 4000")
+    logger.info("Server is Running on port 4000")
 })
 
 web.listen(4000, () => {
