c03e91578a14c03a607fd3136e77d585f2e5a84b
root <root@srv511009.hstgr.cloud>
2024-04-19 15:29:16 +0000

    Handle preFlight Network

--- src/application/web.js
+++ src/application/web.js
@@ -21,36 +21,6 @@ web.use(cors(
 
 web.use(express.json());
 
-function socketConnectionRequest(req, res, next) {
-    const headers = {
-        'Access-Control-Allow-Origin': 'https://admin.synchronice.id', // To tell client, it is allowed to access this resource from any origin
-        'Cache-Control': 'no-cache', // To tell client, it is not a cacheable response
-        'Content-Type': 'text/event-stream', // To tell client, it is event stream
-        'Connection': 'keep-alive', // To tell client, not to close connection
-    };
-    res.writeHead(200, headers);
-    res.write('data: Connection Established, We\'ll now start receiving messages from the server.\n')
-    socket = res
-    console.log('New connection established')
-}
-
-function publishMessageToConnectedSockets(data) {
-    socket.write(`data: ${data}\n`);
-}
-
-web.get('/', (req, res) => {
-    const status = {
-        "status": "Server is running."
-    }
-
-    res.send(status);
-})
-web.get('/socket-connection-request', socketConnectionRequest);
-web.get('/send-message-to-client', (req, res, next) => {
-    publishMessageToConnectedSockets(`Sukses terus Saptakarya at ${new Date()}`)
-    res.sendStatus(200)
-});
-
 web.use(publicRouter); 
 
 web.use(errorMiddleware);
